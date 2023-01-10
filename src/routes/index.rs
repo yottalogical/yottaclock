46c32b386e7a8f2bd5141ac6bc27e11222ce9b0f
yottalogical <email@yottalogical.com>
2023-01-09 23:34:13 -0500

    Add Bootstrap to index.html

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -8,6 +8,7 @@ use axum::{
     extract::Extension,
     response::{Html, IntoResponse, Redirect},
 };
+use chrono::Duration;
 use reqwest::Client;
 use sqlx::PgPool;
 
@@ -31,6 +32,8 @@ impl From<toggl::Goal> for Goal {
 #[template(path = "index.html")]
 pub struct Index {
     pub total_debt: HumanDuration,
+    pub daily_max: HumanDuration,
+    pub percentage: i64,
     pub goals: Vec<Goal>,
 }
 
@@ -39,16 +42,22 @@ pub async fn get(
     Extension(pool): Extension<PgPool>,
     Extension(client): Extension<Client>,
 ) -> InternalResult<impl IntoResponse> {
-    Ok(
-        if let Some((goals, total_debt)) = calculate_goals(user_key, pool, client).await? {
-            let template = Index {
-                total_debt: HumanDuration(total_debt),
-                goals: goals.into_iter().map(|g| g.into()).collect(),
-            };
+    let record = sqlx::query!("SELECT daily_max FROM users WHERE user_key = 1")
+        .fetch_one(&pool)
+        .await?;
 
-            Html(template.render()?).into_response()
-        } else {
-            Redirect::to("/project/new/").into_response()
-        },
-    )
+    let daily_max = Duration::seconds(record.daily_max);
+
+    if let Some((goals, total_debt)) = calculate_goals(user_key, pool, client).await? {
+        let template = Index {
+            total_debt: HumanDuration(total_debt),
+            daily_max: HumanDuration(daily_max),
+            percentage: 100 - (total_debt.num_seconds() * 100 / daily_max.num_seconds()),
+            goals: goals.into_iter().map(|g| g.into()).collect(),
+        };
+
+        Ok(Html(template.render()?).into_response())
+    } else {
+        Ok(Redirect::to("/project/new/").into_response())
+    }
 }
